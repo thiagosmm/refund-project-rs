16d619c09918ed36d5fe0438276385f4c265c0e5
thiagosmm <thiagosmmonteiro@gmail.com>
2025-02-03 18:27:37 -0300

    formatação do amount

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 //select forms elements
 const amount = document.getElementById("amount")
 
+//input value to accept digits only
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
-  amount.value = value
+
+  //transform value in cents
+  value = Number(value) / 100
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+//formating to BRL currency
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  return value
 }
